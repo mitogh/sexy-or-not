46651803c42a2ee445e230b924c61de0b1b20db6
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-02-26 15:54:28 -0600

    Basic JS to retrive data and simulate the form send

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-$().click(function(){
-    $.ajax()
+jQuery(document).ready(function(){
+    jQuery(".votes").click(function(){
+        console.log(jQuery("input[name=rate]:checked", this).val());
+        location.reload();
+    });
 });
