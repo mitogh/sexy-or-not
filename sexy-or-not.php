502c783fead8160c8a7885628b81387239ab3eb1
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2014-02-26 15:37:19 -0600

    Action to load JS

--- sexy-or-not.php
+++ sexy-or-not.php
@@ -38,6 +38,15 @@ function options(){
 <?php
 }// Options
 
+// Add the script.js file to the stack of loading files
+add_action("wp_enqueue_scripts", "sexy_scripts");
+function sexy_scripts(){
+    wp_enqueue_script("jquery");
+    wp_register_script("sexy-script", plugins_url("/js/script.js", __FILE__), array(), null);
+    wp_enqueue_script("sexy-script");
+}
+
+// Add the file style.css as the base styles
 function sexy_styles(){
     wp_register_style("sexy-style", plugins_url("/css/style.css", __FILE__), array(), null, "all");
     wp_enqueue_style("sexy-style");
